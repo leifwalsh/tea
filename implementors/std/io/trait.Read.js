891e8c63cfb41133ca026e39a87a8c49c9dbc15b
Leif Walsh <leif.walsh@gmail.com>
2015-03-05 01:03:50 +0000

    rebuild pages at df47653

---
+++ implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tea'] = ["<a class='stability Unstable' title='Unstable: this new I/O module is still under active development and APIs are subject to tweaks fairly regularly'></a>impl&lt;R: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='tea/io/struct.Reader.html' title='tea::io::Reader'>Reader</a>&lt;R&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['tea'] = ["<a class='stability Unstable' title='Unstable: this new I/O module is still under active development and APIs are subject to tweaks fairly regularly'></a>impl&lt;W: <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='tea/io/struct.Writer.html' title='tea::io::Writer'>Writer</a>&lt;W&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
